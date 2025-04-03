cf802e2413dad8128eaad5a11beed32a2d78b50a
Santacruzluis <luisantacruzkids@gmail.com>
2025-04-03 15:42:50 -0400

    FIX and ADD se cambio que la impresion sea para ventas y aun falta que funcione lo de imprimir

--- custom_print/models/__init__.py
+++ custom_print/models/__init__.py
@@ -1,4 +1,4 @@
 from . import res_config_settings
-from . import account_move
+from . import sale_order
 from . import res_company
 
--- custom_print/models/sale_order.py
+++ custom_print/models/sale_order.py
@@ -1,7 +1,7 @@
 from odoo import models, fields, api
 
-class AccountMove(models.Model):
-    _inherit = 'account.move'
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
     
     custom_print_enabled = fields.Boolean(
         compute='_compute_custom_print_enabled',
@@ -23,22 +23,30 @@ class AccountMove(models.Model):
     
     @api.depends('tax_totals')
     def _compute_amount_by_group(self):
-        """Compute amount by group for tax totals"""
-        for move in self:
-            if move.tax_totals and 'groups_by_subtotal' in move.tax_totals:
+        """Compute amount by group for tax totals (adaptado para cotizaciones)"""
+        for order in self:
+            if order.tax_totals and 'groups_by_subtotal' in order.tax_totals:
                 amount_by_group = []
-                for subtotal in move.tax_totals['groups_by_subtotal'].values():
+                for subtotal in order.tax_totals['groups_by_subtotal'].values():
                     for group in subtotal:
                         amount_by_group.append((
                             group['tax_group_name'],
                             group['tax_group_amount'],
                             group['tax_group_base_amount']
                         ))
-                move.amount_by_group = amount_by_group
+                order.amount_by_group = amount_by_group
             else:
-                move.amount_by_group = []
+                order.amount_by_group = []
     
-    def action_print_custom(self):
-        """Acción para imprimir la factura con formato personalizado"""
+    def action_print_custom_quotation(self):
+        """Acción para imprimir la cotización con formato personalizado"""
         self.ensure_one()
-        return self.env.ref('custom_print.action_report_custom_invoice').report_action(self)
+        report = self.env.ref('custom_print.action_report_custom_quotation')
+        paper_size = self.env['ir.config_parameter'].sudo().get_param(
+            'custom_print.paper_size', 'letter')
+        
+        if paper_size == 'half_letter':
+            return report.with_context(
+                paperformat='custom_print.paperformat_half_letter'
+            ).report_action(self)
+        return report.report_action(self)
